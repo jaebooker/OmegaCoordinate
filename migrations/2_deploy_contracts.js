dcaca69026304822fd30afb0b4be91322264c153
jaebooker <jaesonbooker@gmail.com>
2018-10-18 00:08:07 -0700

    added deadline contract and artifact

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
 var Authentication = artifacts.require("./Authentication.sol");
 var Projects = artifacts.require("./Projects.sol");
+var Deadlines = artifacts.require("./Deadlines.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
@@ -10,4 +11,5 @@ module.exports = function(deployer) {
   deployer.link(Killable, Authentication);
   deployer.deploy(Authentication);
   deployer.deploy(Projects);
+  deployer.deploy(Deadlines);
 };
